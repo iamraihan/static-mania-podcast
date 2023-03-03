9a70da789f79d744ec1de6b0d92e5e517e7f06fb
iamraihan <trueraihan@gmail.com>
2023-03-03 18:45:48 +0600

    navbar done

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import logo from "../assets/images/hero/Logo.svg";
 const Navbar = () => {
   return (
-    <div className="max-w-[1272px] mx-auto px-3 xl:px-0">
-      <h1>navbar</h1>
+    <div className="max-w-[1272px] mx-auto px-3 xl:px-0 flex justify-between py-[23px]">
+      <div>
+        <Link to="/">
+          {" "}
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <Link to="#">
+        <p className="body-text leading-7">About</p>
+      </Link>
     </div>
   );
 };
