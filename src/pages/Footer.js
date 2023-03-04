02999f23ba6f70d0e8738b9e5777a19903d2426a
trueraihan@gmail.comgit config --global user.name trueraihan@gmail.comgit config --global user.name Raihan <trueraihan@gmail.com>
2023-03-04 08:18:47 +0600

    footer done

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
   return (
     <div className="">
       <div className=" ">
-        <div className="bg-primary container mx-auto ">
+        {/* <div className="bg-primary container mx-auto  ">
           <div className="p-[72px]">
             <h3 className="heading-3 text-white text-center mb-16">
               Our Sponsors
@@ -38,9 +38,33 @@ const Footer = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="bg-dark pb-24  pt-[192px]">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container mx-auto">
+          <div className="bg-primary container mx-auto mt-[-420px] lg:mt-[-372px]">
+            <div className="p-[72px]">
+              <h3 className="heading-3 text-white text-center mb-16">
+                Our Sponsors
+              </h3>
+              <div className="flex justify-between flex-wrap lg:flex-nowrap gap-[84px]">
+                <div>
+                  <img src={logoIpsum_1} alt="" />
+                </div>
+                <div>
+                  <img src={logoIpsum_2} alt="" />
+                </div>
+                <div>
+                  <img src={logoIpsum_3} alt="" />
+                </div>
+                <div>
+                  <img src={logoIpsum_4} alt="" />
+                </div>
+                <div>
+                  <img src={logoIpsum_5} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container mx-auto mt-20">
             <div>
               <img src={brand} alt="" />
             </div>
